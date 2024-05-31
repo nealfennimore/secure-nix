a24dbbe9c19e2c37b47ba8fc73054de5b681dce0
Neal Fennimore <hi@neal.codes>
2024-05-31 11:14:07 -0400

    Update audit rules

--- modules/security/audit.nix
+++ modules/security/audit.nix
@@ -1,8 +1,8 @@
 { lib, ... }:
 let
   rules = builtins.readFile (builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/Neo23x0/auditd/8ba9a8397b221aed85c66d3f1c5c4e336dd78a57/audit.rules";
-    sha256 = "sha256:1vq57h3j856kfxbq4mcflxlvxk3dvqa9vv5qhf2d6wzjqx6w357i";
+    url = "https://raw.githubusercontent.com/Neo23x0/auditd/d4a373a81ce3ea1930aa007ee32aa2f68c0b4db0/audit.rules";
+    sha256 = "sha256:0lj63zns55wk947xb47yak1sqzs21lm2bxzy8ncylzc546n20mh8";
   });
 in
 {
