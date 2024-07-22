326c90059396bb29982d32387ebd99afc330576a
Neal Fennimore <hi@neal.codes>
2024-07-22 09:03:22 -0400

    Update with more ssh hardening

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -3,9 +3,15 @@
   services.openssh = {
     enable = lib.mkDefault false;
     settings = {
-      PermitRootLogin = lib.mkDefault "no";
-      PasswordAuthentication = lib.mkDefault false;
+      AllowAgentForwarding = lib.mkDefault "no";
+      AllowTcpForwarding = lib.mkDefault "no";
+      ClientAliveCountMax = lib.mkDefault 2;
       KbdInteractiveAuthentication = lib.mkDefault false;
+      MaxAuthTries = lib.mkDefault 3;
+      MaxSessions = lib.mkDefault 2;
+      PasswordAuthentication = lib.mkDefault false;
+      PermitRootLogin = lib.mkDefault "no";
+      TcpKeepAlive = lib.mkDefault "no";
     };
   };
 }
