da1ae12249fd3bb007add9293da606f328b5ce10
Neal Fennimore <hi@neal.codes>
2024-07-22 09:54:45 -0400

    Add in blacklist modules

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,4 +1,13 @@
 { pkgs, lib, ... }:
 {
   boot.kernelPackages = lib.mkOrder 1499 pkgs.linuxKernel.packages.linux_6_9_hardened;
+  boot.kernelModules = lib.mkDefault [ ];
+  boot.initrd.includeDefaultModules = lib.mkDefault false;
+  boot.initrd.kernelModules = lib.mkDefault [ ];
+  boot.blacklistedKernelModules = lib.mkDefault [
+    "dccp"
+    "sctp"
+    "rds"
+    "tipc"
+  ];
 }
