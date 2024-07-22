67ad4b65ec25f2dc6f78c1387893963b9bc126fd
Neal Fennimore <hi@neal.codes>
2024-07-22 10:19:16 -0400

    Increase hashing rounds

--- modules/security/login.nix
+++ modules/security/login.nix
@@ -5,6 +5,7 @@
       PASS_MAX_DAYS = lib.mkDefault 365;
       PASS_MIN_DAYS = lib.mkDefault 1;
       PASS_WARN_AGE = lib.mkDefault 30;
+      SHA_CRYPT_MIN_ROUNDS = lib.mkDefault 10000;
     };
   };
 }
